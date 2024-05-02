e05f4f469bf6c065f965256baf346bc36dd7b18d
Adrià Cabrera <adriacabrera2007@gmail.com>
2024-05-02 20:06:52 +0200

    reforce middleware signin and add images index page via cdn

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,88 @@
-interface Values {
-  redirect: any;
-  url: any;
-  cookies: any;
-}
+import { defineMiddleware } from "astro:middleware";
+import { supabase } from "./lib/supabase";
+import micromatch from "micromatch";
 
-export async function onRequest(Values: any, next: any) {
-  const refreshToken = Values.cookies.get("sb-refresh-token");
-  const accessToken = Values.cookies.get("sb-access-token");
-  console.log("middleware ejecutandose", Values.url.pathname);
-  if (Values.url.pathname.includes("/dashboard")) {
-    console.log("middleware verificando pagina");
-    if (!accessToken || !refreshToken) {
-      console.log("sesion no iniciada");
+const protectedRoutes = ["/dashboard(|/)"];
+const redirectRoutes = ["/auth/signin(|/)"];
+const proptectedAPIRoutes: any = [];
 
-      return Values.redirect("/auth/signin");
+export const onRequest = defineMiddleware(
+  async ({ locals, url, cookies, redirect }, next) => {
+    if (micromatch.isMatch(url.pathname, protectedRoutes)) {
+      const accessToken = cookies.get("sb-access-token");
+      const refreshToken = cookies.get("sb-refresh-token");
+
+      if (!accessToken || !refreshToken) {
+        return redirect("/auth/signin");
+      }
+
+      const { data, error } = await supabase.auth.setSession({
+        refresh_token: refreshToken.value,
+        access_token: accessToken.value,
+      });
+
+      if (error) {
+        cookies.delete("sb-access-token", {
+          path: "/",
+        });
+        cookies.delete("sb-refresh-token", {
+          path: "/",
+        });
+        return redirect("/signin");
+      }
+
+      //locals.email = data.user?.email!;
+      cookies.set("sb-access-token", data?.session?.access_token!, {
+        sameSite: "strict",
+        path: "/",
+        secure: true,
+      });
+      cookies.set("sb-refresh-token", data?.session?.refresh_token!, {
+        sameSite: "strict",
+        path: "/",
+        secure: true,
+      });
     }
-    console.log("sesion iniciada");
-  }
-  return next();
-}
+
+    if (micromatch.isMatch(url.pathname, redirectRoutes)) {
+      const accessToken = cookies.get("sb-access-token");
+      const refreshToken = cookies.get("sb-refresh-token");
+
+      if (accessToken && refreshToken) {
+        return redirect("/dashboard");
+      }
+    }
+
+    if (micromatch.isMatch(url.pathname, proptectedAPIRoutes)) {
+      const accessToken = cookies.get("sb-access-token");
+      const refreshToken = cookies.get("sb-refresh-token");
+
+      // Check for tokens
+      if (!accessToken || !refreshToken) {
+        return new Response(
+          JSON.stringify({
+            error: "Unauthorized",
+          }),
+          { status: 401 },
+        );
+      }
+
+      // Verify the tokens
+      const { error } = await supabase.auth.setSession({
+        access_token: accessToken.value,
+        refresh_token: refreshToken.value,
+      });
+
+      if (error) {
+        return new Response(
+          JSON.stringify({
+            error: "Unauthorized",
+          }),
+          { status: 401 },
+        );
+      }
+    }
+
+    return next();
+  },
+);
